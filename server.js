8e84cf5aee1ce579b2625fd0e1e3aa5261baa8da
didericis <didericis@gmail.com>
2015-10-08 00:53:46 -0400

    Switch from express to regular http

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
 var twilio = require('twilio');
 var nodemailer = require('nodemailer');
-
+var qs= require('querystring');
 var http = require('http');
+
 var twilioToken = process.env.TWILIO_TOKEN;
 
 http.createServer(function(req, res) {
-    if (twilio.validateExpressRequest(req, twilioToken, {url: config.webhook})) {
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        var body = req.param('Body').trim();
-        var to = req.param('To');
-        var from = req.param('From');
-        console.log(body);
-        // forwardSms(body);
-        res.end();
-    }
-    else {
-        res.writeHead(403, 'Forbidden', {'Content-Type': 'text/html'});
-        res.end();
+    if (req.method == 'POST') {
+        var body = '';
+
+        req.on('data', function (data) {
+            body += data;
+        });
+
+        req.on('end', function() {
+            var postData = qs.parse(body);
+
+            if (twilio.validateRequest(twilioToken, req.headers['x-twilio-signature'], 'http://67.83.190.74:3555/', postData)){
+                var message = postData.Body.trim();
+                var to = postData.To;
+                var from = postData.From
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write('Working');
+                
+                console.log('New text from ' + from);
+                console.log('"' + message + '"');
+                // forwardSms(body);
+                res.end();
+            } else {
+                console.log('INVALID REQUEST');
+                res.writeHead(403, 'Forbidden', {'Content-Type': 'text/html'});
+                res.end();
+            }
+        });
     }
 }).listen(3555);
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<html><body>HI</body></html>');
-}).listen(80);
+//http.createServer(function(req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.end('<html><body>HI</body></html>');
+//}).listen(80);
 
 //routes['/twilio'] = function(req, res) {
 //    if (twilio.validateExpressRequest(req, config.authToken)) {
